a10f82efb8f25ac9541f2b63c961c4e845858a9e
Harry Kimpel <harrykimpel@hotmail.com>
2024-10-08 17:00:35 +0200

    Adding Google Gemini AI example app
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

---
+++ 2-python-gemini-newrelic/leveltwo.py
@@ -0,0 +1,45 @@
+# import the New Relic Python Agent
+import newrelic.agent
+import os
+from flask import Flask, render_template, request
+import google.generativeai as genai
+
+genai.configure(api_key=os.environ["API_KEY"])
+
+app = Flask(__name__)
+
+# initialize the New Relic Python agent
+newrelic.agent.initialize('newrelic.ini')
+
+def chatCompletion(prompt):
+    # completion = client.chat.completions.create(
+    #     model="gpt-3.5-turbo",
+    #     temperature=0.8,
+    #     max_tokens=256,
+    #     messages=[
+    #         {"role": "user", "content": prompt}
+    #     ])
+    # return completion.choices[0].message.content
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    response = model.generate_content(
+        prompt,
+        generation_config=genai.types.GenerationConfig(
+            temperature=1.0,
+        ),
+    )
+    return response.candidates[0].content.parts[0].text
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/prompt", methods=["POST"])
+def prompt():
+    input_prompt = request.form.get("input")
+    output_prompt = chatCompletion(input_prompt)
+    return render_template("index.html", output=output_prompt)
+
+# make the server publicly available via port 5004
+# flask --app levelsix.py run --host 0.0.0.0 --port 5004
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", debug=True, port=5004)
