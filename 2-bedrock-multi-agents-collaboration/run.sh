6efa75381e23da9696e5f2dcf028b00324c4034d
Harry Kimpel <harrykimpel@hotmail.com>
2025-05-19 23:03:19 +0300

    Adding and updating more examples
    Signed-off-by: Harry Kimpel <harrykimpel@hotmail.com>

--- 2-bedrock-multi-agents-collaboration/run.sh
+++ 2-bedrock-multi-agents-collaboration/run.sh
@@ -1,5 +1,5 @@
 export NEW_RELIC_LICENSE_KEY=YOUR_NEW_RELIC_LICENSE_KEY
 export OTEL_EXPORTER_OTLP_ENDPOINT=https://otlp.nr-data.net
 export OTEL_EXPORTER_OTLP_HEADERS="api-key=$NEW_RELIC_LICENSE_KEY"
-export OTEL_SERVICE_NAME=bedrock-energy-agent"
+export OTEL_SERVICE_NAME="bedrock-energy-agent"
 python3 main.py
